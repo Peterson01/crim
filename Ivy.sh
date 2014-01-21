677802405125774abba88e046ed1b7fb27cfcaaf
Sophie Nolden <sophie_nolden@directbox.com>
2014-01-21 11:11:25 -0500

    New comment in shell script

--- Ivy.sh
+++ Ivy.sh
@@ -1,2 +1,2 @@
-# Display duplicate entries in clumn 2 of a CSV file.
+# Display duplicate entries in clumn 2 of a CSV file. Now learning to use git.
 grep -v Species $1 | cut -d , -f 2 | sort | uniq -c | grep -v 1
